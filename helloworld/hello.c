e6d4f371a08001ccb57daf0f0abc0377e2102ebe
Nate Brune <natebrune@gmail.com>
2015-01-11 12:28:38 -0500

    Removed eudyptula stuff

--- helloworld/hello.c
+++ helloworld/hello.c
@@ -3,12 +3,12 @@
 
 int init_module(void)
 {
-  printk(KERN_DEBUG "Hello little!\n");
+  printk(KERN_DEBUG "Hello World!\n");
   return 0;
 }
 
 void cleanup_module(void)
 {
-  printk(KERN_DEBUG "Goodbye little!\n");
+  printk(KERN_DEBUG "Goodbye World!\n");
 }
 MODULE_LICENSE("GPL");
